db0fcc211dc076031223409ebcd92ee54291dd3d
ahmadammarjr <110498902+KamenYaiba@users.noreply.github.com>
2025-03-07 07:08:01 +0300

    add mode switching logic

--- scc/scc.ino
+++ scc/scc.ino
@@ -1,11 +1,31 @@
-uint8_t mode; // 0: offensive, 1: defensive, 2: random
+#include <xmotionV3.h>
+#include "scc.h"
 
+uint8_t mode; // 1: offensive, 2: defensive, 3: random
 
 void (*reactionToDetection) ();
 
 
 void setup() {
-  // put your setup code here, to run once:
+  Serial.begin(9600); // delete before deployment
+  Serial.println("STARTTING...");
+
+  pinMode(DipSwitch1, INPUT);
+  pinMode(DipSwitch3, INPUT);
+
+  mode = digitalRead(DipSwitch1) + 2 * digitalRead(DipSwitch3);
+
+  switch(mode) {
+    case OFFENSIVE: reactionToDetection = attack;
+      break;
+    case DEFENSIVE: reactionToDetection = avoid;
+      break;
+    case RANDOM: reactionToDetection = unexpectedResponse;
+      break;
+    default: Serial.println("ERROR: No mode selected");
+  }
+
+  
 
 }
 
@@ -13,3 +33,42 @@ void loop() {
   // put your main code here, to run repeatedly:
 
 }
+
+
+
+
+void attack() {
+
+}
+
+
+
+void avoid() {
+
+}
+
+
+void unexpectedResponse() {
+  if(random(0, 1))
+    attack();
+  else
+    avoid();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
