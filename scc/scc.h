b39a64d200e6e8af38bc8e39ace4d8b563d0fe54
ahmadammarjr <110498902+KamenYaiba@users.noreply.github.com>
2025-03-07 07:26:08 +0300

    add sensors

--- scc/scc.h
+++ scc/scc.h
@@ -1,9 +1,19 @@
-#define OFFENSIVE 1
-#define DEFENSIVE 2
-#define RANDOM 3
 
+//circuit
+#define START A0
 #define DipSwitch1 5
 #define DipSwitch3 7
 
-#define FRONT_LINE_SENSOR
-#define 
+#define RIGHT_LINE_SENSOR 0
+#define LEFT_LINE_SENSOR 1
+#define LONG_IR 2
+#define SHORT_IR 4
+
+
+
+
+
+//custom
+#define OFFENSIVE 1
+#define DEFENSIVE 2
+#define RANDOM 3
