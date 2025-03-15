500629ea60d3894451407bce8692a11320a9f509
ahmadammarjr <110498902+KamenYaiba@users.noreply.github.com>
2025-03-15 07:57:31 +0300

    add the u version

---
+++ scc_u/scc_u.h
@@ -0,0 +1,49 @@
+#ifndef SCC_U_H
+#define SCC_U_H
+
+
+#include <xmotionV3.h>
+
+
+#define degrees(x) ((x) / (6.0))
+
+#define EDGE_S_L 0
+#define EDGE_S_R 1
+#define REAR_S 2
+#define LEFT_S 3
+#define RIGHT_S 4
+#define SWITCH_1 5
+#define SWITCH_2 6
+#define SWITCH_3 7
+#define SWITCH_180 A1
+#define SEARCH_S A2
+#define DISTANCE_S_T A4
+#define DISTANCE_S_E A5
+
+
+#define RIGHT 0
+#define LEFT 1
+
+
+#endif
+
+
+enum State {
+  IDLE,
+  QUICK_SEARCHING,
+  SEARCHING,
+  LOCKED_IN,
+  APPROACHING,
+  SIDE_ATTACKING,
+  PUSHING,
+  ROTATING,
+  EVADING
+};
+
+
+
+
+
+
+
+
---
+++ scc_u/scc_u.ino
@@ -0,0 +1,112 @@
+#include "scc_u.h"
+
+State state = IDLE;
+unsigned long lastUpdate;
+int timer;
+
+void setup() {
+  pinMode(EDGE_S_L, INPUT);
+  pinMode(EDGE_S_R, INPUT);
+  pinMode(REAR_S, INPUT);
+  pinMode(LEFT_S, INPUT);
+  pinMode(RIGHT_S, INPUT);
+  pinMode(SWITCH_1,  INPUT_PULLUP);
+  pinMode(SWITCH_2,  INPUT_PULLUP);
+  pinMode(SWITCH_3,  INPUT_PULLUP);
+  pinMode(SWITCH_180,  INPUT_PULLUP);
+  pinMode(SEARCH_S, INPUT);
+  pinMode(DISTANCE_S_T, INPUT);
+  pinMode(DISTANCE_S_E, INPUT);
+
+  delay(4900);
+  if(digitalRead(SWITCH_180))
+    xmotion.Right0(100, degrees(180));
+
+  state = QUICK_SEARCHING;
+  
+}
+
+void loop() {
+  // check edge
+
+  switch(state) {
+
+    case QUICK_SEARCHING:
+      if(digitalRead(SEARCH_S)) {
+        changeState(LOCKED_IN);
+      }
+
+      elif(digitalRead(LEFT_S)) {
+        rotate(degrees(90), LEFT);
+        changeState(ROTATING);
+      }
+
+      elif(digitalRead(RIGHT_S)) {
+        rotate(degrees(90), RIGHT);
+        changeState(ROTATING);
+      }
+
+      elif(digitalRead(REAR_S)) {
+        rotate(180, RIGHT);
+        changeState(ROTATING);
+
+      }
+      break;
+
+  case SEARCHING:
+    if(digitalRead(SEARCH_S) == HIGH) {
+      xmotion.StopMotors(0);
+      changeState(LOCKED_IN);
+    }
+
+      break;
+
+  case LOCKED_IN:
+      break;
+  case APPROACHING:
+      break;
+  case SIDE_ATTACKING:
+      break;
+  case PUSHING:
+      break;
+
+  case ROTATING:
+    if(millis() - lastUpdate > timer) {
+      xmotion.StopMotors(0);
+      changeState(QUICK_SEARCHING);
+    }
+
+      break;
+  case EVADING:
+      break;
+    default: break;
+  
+
+  }
+
+}
+
+
+
+
+
+void changeState(State s) {
+  state = s;
+  lastUpdate = millis();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
