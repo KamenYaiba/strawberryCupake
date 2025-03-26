962122019471e26bc8897db8d225253ae1607052
ahmadammarjr <110498902+KamenYaiba@users.noreply.github.com>
2025-03-27 00:59:51 +0300

    Update tests.ino

--- tests/tests.ino
+++ tests/tests.ino
@@ -1,44 +1,56 @@
-// Define pins for the HC-SR04
-const int trigPin = A1;
-const int echoPin = 3;
+#include <xmiomod.h>
 
-// Variables to hold distance and duration
-long duration;
-int distance;
+#define DISTANCE_S_T A1
+#define DISTANCE_S_E 3
+
+
 
 void setup() {
-  // Start serial communication for debugging
-  Serial.begin(9600);
-
-  // Set the trigPin as an output and echoPin as an input
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
-  pinMode(0, INPUT);
+  pinMode(DISTANCE_S_T, OUTPUT);
+  pinMode(DISTANCE_S_E, INPUT);
 }
 
 void loop() {
-  // Clear the trigPin by setting it LOW
-  digitalWrite(trigPin, LOW);
-  delayMicroseconds(2);
+  testUS();
+}
+
+
+
+void testMotors(int dir, uint8_t speed) {
+  if(dir)
+    x_forward(speed);
+  else
+    x_backward(speed);
+}
+
+
+
+
+
+void testUS() {
+  Serial.print("time: ");
+  Serial.println(getDistance());
   
-  // Set the trigPin HIGH for 10 microseconds to trigger the ultrasonic pulse
-  digitalWrite(trigPin, HIGH);
-  delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
-  
-  // Read the echoPin, returns the time it took for the pulse to return
-  duration = pulseIn(echoPin, HIGH);
-  
-  // Calculate the distance based on the duration
-  // Distance = (duration / 2) * speed of sound (in cm/us)
-  // Speed of sound is approximately 0.034 cm/us
-  distance = duration * 0.034 / 2;
-  
-  // Print the distance to the Serial Monitor
-  Serial.print("Distance: ");
-  Serial.print(distance);
-  Serial.println(" cm");
-  
-  // Wait for a while before taking another measurement
   delay(500);
 }
+
+
+int getDistance() {
+  digitalWrite(DISTANCE_S_T, LOW);
+  delayMicroseconds(2);
+  
+  digitalWrite(DISTANCE_S_T, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(DISTANCE_S_T, LOW);
+  
+  // 58.3 us = 1 cm
+  int distance = pulseIn(DISTANCE_S_E, HIGH, 9000);
+  if(distance)
+    return distance;
+
+  return 35333;
+} 
+
+
+
+
